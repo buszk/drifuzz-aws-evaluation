cfc762475a4db412576c7443d09fa684342ca44b
Zekun Shen <17345356+buszk@users.noreply.github.com>
2021-05-08 00:39:52 +0000

    fix and update

--- list_all.sh
+++ list_all.sh
@@ -3,7 +3,7 @@
 result=$(aws ec2 describe-instances)
 ids=($(echo $result | jq -r ".Reservations[].Instances[].InstanceId"))
 states=($(echo $result | jq -r ".Reservations[].Instances[].State.Name"))
-ips=($(echo $result | jq -r ".Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp"))
+ips=($(echo $result | jq -r ".Reservations[].Instances[].PublicIpAddress"))
 len=${#ids[@]}
 
 # echo len: $len
@@ -11,4 +11,4 @@ for i in $(seq 0 $(($len-1))); do
     echo ${ids[$i]} ${states[$i]}
 done
 echo "IP addresses in use:"
-echo $ips
+echo $result | jq -r ".Reservations[].Instances[].PublicIpAddress"
--- run_on_aws.sh
+++ run_on_aws.sh
@@ -9,14 +9,21 @@ concolic_configs=(1 0)
 model_configs=(1 0)
 niter=4
 
+function finish {
+    echo
+    echo "Terminating. Please wait..."
+    ./terminate_all.sh
+}
+trap finish EXIT
+
 rm -f run_instances.log
 mkdir -p generated
 for i in $(seq $niter); do
     for t in ${targets[*]}; do
         for cc in ${concolic_configs[*]}; do
             for mc in ${model_configs[*]}; do
-                echo $t $cc $mc
-                echo "${t}_${cc}_${mc}.sh"
+                # echo $t $cc $mc
+                echo "Starting instance running ${t}_${cc}_${mc}.sh"
                 userinput=generated/${t}_${cc}_${mc}_${i}.sh
                 sed "s/{target}/$t/g;s/{concolic}/$cc/g;s/{model}/$mc/g;s/{iteration}/$i/g" \
                         base.sh.template > $userinput
@@ -28,7 +35,7 @@ for i in $(seq $niter); do
                     --security-group-ids sg-09fc27778f02c34a0 \
                     --instance-market-options file://spot-options.json \
                     --block-device-mappings file://mapping.json \
-                    --user-data file://$userinput 2>&1| tee -a run_instances.log
+                    --user-data file://$userinput 2>&1 >run_instances.log
                 if [ $? -ne 0 ]; then
                     echo "Launch instance encountered error"
                     echo "tail run_instances.log"
@@ -45,5 +52,5 @@ for i in $(seq $niter); do
 done
 
 sleep 9000
-./template_all.sh
+./terminate_all.sh
 
